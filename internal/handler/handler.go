c64dbc8943bb4fc3b27c26dc9ed63394cfb71a1b
ziyangxing <ziyangxing@github.com>
2026-05-26 23:25:10 +0800

    fix: show both inline preview and clickable external open link for files

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -645,7 +645,7 @@ func (h *Handler) handleFileAttachment(msg *openwechat.Message, sender, title, d
 	}
 
 	var content strings.Builder
-	content.WriteString(fmt.Sprintf("![[%s|📂 %s]]\n\n", relPath, fileName))
+	content.WriteString(fmt.Sprintf("![[%s]]\n\n[[%s|📂 点击用外部程序打开]]\n\n", relPath, relPath))
 	if desc != "" {
 		content.WriteString(fmt.Sprintf("> %s\n\n", desc))
 	}
